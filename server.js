27ba42e25917605157772d5a9be3bfdd3d1a8932
mtlira <matheusribeiro10@gmail.com>
2022-04-02 11:57:11 -0300

    comentarios

--- server.js
+++ server.js
@@ -4,23 +4,31 @@ const bcrypt = require('bcrypt')
 
 app.use(express.json())
 
+// Mock do banco de dados do backend para testes isolados no front
 const users = []
 
+// Para teste somente!
 app.get('/users', (req, res) => {
   res.json(users)
 })
 
+// Cadastro do usuario (Rota no Frontend)
 app.post('/users', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const hashedPassword = await bcrypt.hash(req.body.password, 10) // criptografia da senha
     const user = { name: req.body.name, password: hashedPassword }
-    users.push(user)
-    res.status(201).send()
+    users.push(user) // Mandar POST em /cadastrar do backend
+    /* 
+    Depois de mandar o POST pro backend, entra a parte de 
+    receber o request com o id_login do usuario cadastrado e guarda-la no JWT
+    */
+    res.status(201).send() // Cadastro com sucesso
   } catch {
-    res.status(500).send()
+    res.status(500).send() // Erro no cadastro
   }
 })
 
+// Backend
 app.post('/users/login', async (req, res) => {
   const user = users.find(user => user.name === req.body.name)
   if (user == null) {
